2f9012c650ad0a777eaab2488e7d1278a351c433
Fanxu Meng <2473816m@student.gla.ac.uk>
2020-01-31 05:17:53 +0000

    added a .gitinore file

---
+++ rango/urls.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Author: Fanxu Meng
+# All rights reserved (C) 2019 by Fanxu Meng
+
+from django.urls import path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('',views.index, name='index'),
+    path('',views.index, name='about'),
+]
